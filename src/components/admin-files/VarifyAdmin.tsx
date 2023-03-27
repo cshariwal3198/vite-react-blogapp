02013bbb7982916f96decb8734a9d181cb797da4
cshariwal3198 <cshariwal3198@gmail.com>
2023-03-27 11:50:45 +0530

    new blog addition complete

--- src/components/admin-files/VarifyAdmin.tsx
+++ src/components/admin-files/VarifyAdmin.tsx
@@ -1,4 +1,5 @@
 import React, { FormEventHandler, useRef, useState } from "react";
+import '../styles/VarifyAdmin.css'
 import { Link } from "react-router-dom";
 
 // function ReturnPageLink({isBoolean}: {isBoolean:boolean}) {
@@ -50,7 +51,13 @@ export default function VarifyAdmin() {
   }
 
   return (
-    <div>
+    <div className="varifyadmin-div">
+      <div>
+        <h4>Welcome to website</h4>
+        <img src="https://media.istockphoto.com/id/1426988809/photo/security-password-login-online-concept-hands-typing-and-entering-username-and-password-of.jpg?b=1&s=170667a&w=0&k=20&c=AJD5Wv30lmyILccJyMpQGhkmh0VhZ5WNDtk53MO1OVM=" height='360px' width={530}/>
+      </div>
+      <div className="login-div">
+        <h5>Admin Login</h5>
       <form onSubmit={(event)=>varifyCredentials(event)}>
         <div>
           <input
@@ -82,6 +89,7 @@ export default function VarifyAdmin() {
         ) : (
           "Entered details are wrong...!! Please try again"
         )}
+      </div>
       </div>
 
       {/* {isRightAdmin? <ReturnPageLink isBoolean={isRightAdmin}/> : ''}  */}
--- src/components/new-blog/CreateBlog.tsx
+++ src/components/new-blog/CreateBlog.tsx
@@ -1,41 +1,73 @@
 import React, {useRef} from 'react';
-import './CreateBlog.css'
+import './CreateBlog.css';
+import { useNavigate } from "react-router-dom";
 
 function CreateBlog() {
 
+  const navigate = useNavigate()
+
   const authorName = useRef<HTMLInputElement>(null)
+  const authorMail = useRef<HTMLInputElement>(null)
+  const authorPassword = useRef<HTMLInputElement>(null)
+  const heading = useRef<HTMLInputElement>(null)
+  const blogPic = useRef<HTMLInputElement>(null)
+  const content = useRef<HTMLTextAreaElement>(null)
+  const publishDate = useRef<HTMLInputElement>(null)  
+
+  function createNewBlog(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault();
+
+    const newBlogObject = {
+      authorName: authorName.current?.value,
+      email: authorMail.current?.value,
+      password: authorPassword.current?.value,
+      heading: heading.current?.value,
+      picture: blogPic.current?.value,
+      content: content.current?.value,
+      publishedOn: publishDate.current?.value,
+    }
+
+    fetch('http://localhost:4000/blogData',{
+      method: 'POST',
+      body: JSON.stringify(newBlogObject),
+      headers : {"Content-Type":"application/json"},
+    }).then(()=>{
+      alert('Your new blog has been added successfully.'),
+      navigate('/')
+    })
+  }
 
   return (
     <div className='post-blog'>
-      <br /><h4> Hello, enjoy your writing.</h4><br />
-      <form className='post-form'>
+      <br /><h4> Hello, Share your valuable thoughts</h4><br />
+      <form className='post-form' onSubmit={(event)=>createNewBlog(event)}>
       <div>
-          <label htmlFor="author">Name of author :</label>
-          <input type="text" name='author' placeholder='Enter name' ref={authorName}/>
+          <label htmlFor="author">Name of author : </label>
+          <input type="text" name='author' placeholder='Enter name' ref={authorName} required autoComplete='off'/>
         </div>
       <div>
-          <label htmlFor="email">Author mail :</label>
-          <input type="email" name='email' placeholder='Enter mail'/>
+          <label htmlFor="email">Author mail : </label>
+          <input type="email" name='email' placeholder='Enter mail' ref={authorMail} required autoComplete='off'/>
         </div>
         <div>
-          <label htmlFor="password">password :  </label>
-          <input type="password" name='password' placeholder='Enter a password'/>
+          <label htmlFor="password">password : </label>
+          <input type="password" name='password' placeholder='Enter a password' ref={authorPassword} required autoComplete='off'/>
         </div>
         <div>
-          <label htmlFor="heading">Blog Heading :</label>
-          <input type="text" name='heading' placeholder='Enter heading'/>
+          <label htmlFor="heading">Blog Heading : </label>
+          <input type="text" name='heading' placeholder='Enter heading' ref={heading} required autoComplete='off'/>
         </div>
         <div>
-          <label htmlFor="heading">Tagline :</label>
-          <input type="text" name='tagline' placeholder='Enter a tagline'/>
+        <label htmlFor="picture">Related picture : </label>
+          <input type="text" name='picture' placeholder='paste image link address here' ref={blogPic} autoComplete='off'/>
         </div>
         <div>
-        <label htmlFor="picture">Related picture :</label>
-          <input type="text" name='picture' placeholder='paste image link address here'/>
+          <label htmlFor="publishDate">Published on : </label>
+          <input type="date" name="publishDate" ref={publishDate} required autoComplete='off'/>
         </div>
         <div>
           <label htmlFor="blogContent">Content here : </label>
-          <textarea name="blogContent" cols={20} rows={10}/>
+          <textarea name="blogContent" cols={20} rows={10} ref={content} required autoComplete='off'/>
         </div>
         <input type="submit" value="Submit" />
       </form>
